036fa6e9b06a5ace97e1ca8da100e4cef821cb86
Ali Daoud <38518048+alidaoud@users.noreply.github.com>
2019-02-04 01:07:02 +0300

    Adding 2019 5th Question

---
+++ Final 2019 5th Question.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+class B1 {
+
+private:
+	int a;
+
+public:
+	B1(int x) {
+		a = x;
+		cout << "Base B1 a=" << a << endl;
+	}
+	B1() {
+		a = 100;
+		cout << "Base B1(no param) a=" << a << endl;
+	}
+	~B1() {
+		cout << "Destructing B1 a=" << a << endl;
+	}
+	int geta() {
+		return a;
+	}
+};
+
+class B2 {
+
+private:
+	int b;
+
+public:
+	B2(int x) {
+		b = x;
+		cout << "Base B2 b=" << b << endl;
+	}
+	~B2() {
+		cout << "Destructing B2 b=" << b << endl;
+	}
+	int getb() {
+		return b;
+	}
+
+};
+
+class D : public B1, public B2 {
+
+private:
+	int c;
+	
+public:
+	D(int x, int y):B2(x), B1() {
+		c = y;
+		cout << "Constructing D (no parameter) c=" << c << endl;
+
+	}
+	D(int x, int y, int z) : B2(x), B1(y){
+
+		c = z;
+		cout << "Constructing D c=" << c << endl;
+	}
+	~D() {
+		cout << "Destructing D c=" << c << endl;
+	}
+	void show() {
+		cout << "a= " << geta() << ", b= " << getb() << ", c= " << c << endl;
+	}
+};
+
+int main() {
+
+	{
+		D ob(1, 3, 5);
+		D ob1(10, 15);
+
+		ob.show();
+
+		B1 bo1(25);
+
+		cout << "a of bo1= " << bo1.geta() << endl;
+		cout << "a of bo= " << ob.geta() << " ,b of bo = " << ob.getb();
+		cout << endl;
+	}
+
+	return 0;
+}
